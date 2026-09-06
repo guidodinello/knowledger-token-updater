b3f32057394d3cc944f66517984573412e4ad896
Guido Dinello <guido.dinello@gmail.com>
2026-09-06 20:50:26 -0300

    chore: migrate to wxt 0.21 config/import APIs

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -1,4 +1,4 @@
-import { storage } from "wxt/storage";
+import { storage } from "#imports";
 import { STORAGE_KEYS, DEFAULT_SETTINGS } from "@/lib/constants";
 import { logger } from "@/lib/utils/logger";
 
